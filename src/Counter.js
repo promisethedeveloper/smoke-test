8b25974a8e0bfb2250a0e6ec40eda4901405e741
Promise Morka <onyekamorka06@gmail.com>
2022-03-24 12:28:59 -0400

    Initial commit

---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+	const [count, setCount] = useState(0);
+	const increment = () => setCount(count + 1);
+	const decrement = () => setCount(count - 1);
+	const reset = () => setCount(0);
+	return (
+		<>
+			<h1>Count is: {count}</h1>
+			<button onClick={increment}>Add</button>
+			<button onClick={decrement}>Subtract</button>
+			<button onClick={reset}>Reset</button>
+		</>
+	);
+};
+
+export default Counter;
